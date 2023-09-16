66045994d641d10bab56f41d3b24397de0ce77db
abdhy <abdhysamoedra030@gmail.com>
2023-09-16 17:53:41 +0700

    customer update

--- customer/register.php
+++ customer/register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="register.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -15,11 +15,8 @@
 <body>
     <div class="kontainer">
         <div class="device">
-            <div class="logo">
-                <h1>Ngojek.</h1>
-            </div>
             <form class="form" action="action/login.php" method="post">
-                <h2>Sign In</h2>
+                <h2>Sign Up</h2>
                 <div class="form-input">
                     <label for="username">Username</label>
                     <input type="text" name="username">
@@ -28,8 +25,27 @@
                     <label for="password">Password</label>
                     <input type="password" name="password">
                 </div>
-                <button type="submit" name="login" value="login">Sign In</button>
-                <h3>Dont have account? <a href="register.php"><span>Sign Up</span></a></h3>
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username">
+                </div>
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                </div>
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username">
+                </div>
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                </div>
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username">
+                </div>
+                <button type="submit" name="login" value="login">Sign Up</button>
             </form>
         </div>
     </div>
