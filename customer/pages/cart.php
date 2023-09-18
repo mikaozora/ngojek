bc321c0a88804c251a6a67d09c1bd50ec4b66291
satmika ozora <mikaozora08@gmail.com>
2023-09-18 14:46:57 +0700

    cart page

---
+++ customer/pages/cart.php
@@ -0,0 +1,86 @@
+<?php
+
+session_start();
+// var_dump($_SESSION["cart"]);
+$total = 0;
+
+if(isset($_POST["checkout"])){
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style/mart.css">
+    <title>Mart</title>
+</head>
+
+<body>
+    <div class="kontainer">
+        <div class="device">
+            <div class="top-bot">
+                <div class="wrap-top">
+                    <div class="wrap-header">
+                        <div class="left">
+                            <a href="mart.php"><img src="../assets/arrow-left.svg" alt=""></a>
+                            <h3>Cart</h3>
+                        </div>
+                    </div>
+                    <div class="wrap-content">
+                        <div class="list-cart">
+                            <?php
+                            if (!empty($_SESSION["cart"])) {
+                                foreach ($_SESSION["cart"] as $key => $value) :
+                                    $total += ($value["quantity"] * $value["price"]);
+                            ?>
+                                    <div class="cart-detail">
+                                        <div class="left">
+                                            <img src="../../uploads/650553800b1a4-makanan.jpg" alt="" width="100%" height="100%">
+                                            <div class="desc-product">
+                                                <h5 class="price">Rp<?= number_format($value["price"], 0, ',', '.') ?></h5>
+                                                <h6 class="title"><?= $value["name"] ?></h6>
+                                            </div>
+                                        </div>
+                                        <div class="right">
+                                            <div class="qty-field">
+                                                <p><?= $value["quantity"] ?></p>
+                                            </div>
+                                            <button type="submit" class="btn-delete">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 18 20" fill="none">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M17.2871 3.24297C17.6761 3.24297 18 3.56596 18 3.97696V4.35696C18 4.75795 17.6761 5.09095 17.2871 5.09095H0.713853C0.32386 5.09095 0 4.75795 0 4.35696V3.97696C0 3.56596 0.32386 3.24297 0.713853 3.24297H3.62957C4.22185 3.24297 4.7373 2.82197 4.87054 2.22798L5.02323 1.54598C5.26054 0.616994 6.0415 0 6.93527 0H11.0647C11.9488 0 12.7385 0.616994 12.967 1.49699L13.1304 2.22698C13.2627 2.82197 13.7781 3.24297 14.3714 3.24297H17.2871ZM15.8058 17.134C16.1102 14.2971 16.6432 7.55712 16.6432 7.48913C16.6626 7.28313 16.5955 7.08813 16.4623 6.93113C16.3193 6.78413 16.1384 6.69713 15.9391 6.69713H2.06852C1.86818 6.69713 1.67756 6.78413 1.54529 6.93113C1.41108 7.08813 1.34494 7.28313 1.35467 7.48913C1.35646 7.50162 1.37558 7.73903 1.40755 8.13594C1.54958 9.89917 1.94517 14.8102 2.20079 17.134C2.38168 18.846 3.50498 19.922 5.13206 19.961C6.38763 19.99 7.68112 20 9.00379 20C10.2496 20 11.5149 19.99 12.8094 19.961C14.4929 19.932 15.6152 18.875 15.8058 17.134Z" fill="#E85454" />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                            <?php endforeach;
+                            } ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrap-bot">
+                    <div class="total-price">
+                        <h3>Total Price</h3>
+                        <h3 class="tp">Rp<?= number_format($total, 0, ',', '.') ?></h3>
+                    </div>
+                    <form action="cart.php" method="post">
+                        <button class="btn-checkout" type="submit" name="checkout">Check out</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ customer/pages/mart.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+include("../../Database/connection.php");
+if (!isset($_SESSION["session_username"])) {
+    header("location: ../index.php");
+    exit();
+}
+$_SESSION["cart"] = NULL;
+$_SESSION["session_merchant_id"] = NULL;
+$sql = "select * from merchant where merchant_type = 'Mart'";
+$res = mysqli_query($conn, $sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style/mart.css">
+    <title>Mart</title>
+</head>
+
+<body>
+    <div class="kontainer">
+        <div class="device">
+            <div class="wrap-header">
+                <div class="left">
+                    <a href="home.php"><img src="../assets/arrow-left.svg" alt=""></a>
+                    <h3>Mart</h3>
+                </div>
+                <div class="right">
+                    <!-- <a href="cart.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.298 10.1692H16.2965C16.7512 10.1692 17.1084 9.79331 17.1084 9.34004C17.1084 8.87571 16.7512 8.51087 16.2965 8.51087H13.298C12.8434 8.51087 12.4862 8.87571 12.4862 9.34004C12.4862 9.79331 12.8434 10.1692 13.298 10.1692ZM19.8579 4.42144C20.5182 4.42144 20.9512 4.65361 21.3842 5.16216C21.8172 5.67071 21.893 6.40038 21.7956 7.0626L20.7672 14.315C20.5724 15.7091 19.4033 16.7362 18.0285 16.7362H6.21855C4.77884 16.7362 3.5881 15.6096 3.46902 14.1503L2.47313 2.09868L0.838573 1.81123C0.405577 1.73385 0.10248 1.30268 0.178254 0.860459C0.254029 0.408289 0.676199 0.108684 1.12002 0.176123L3.70176 0.573016C4.0698 0.640455 4.34043 0.948904 4.3729 1.32479L4.57857 3.80123C4.61105 4.15611 4.8925 4.42144 5.23889 4.42144H19.8579ZM6.04513 18.4835C5.13584 18.4835 4.39975 19.2353 4.39975 20.1639C4.39975 21.0815 5.13584 21.8333 6.04513 21.8333C6.9436 21.8333 7.67969 21.0815 7.67969 20.1639C7.67969 19.2353 6.9436 18.4835 6.04513 18.4835ZM18.2231 18.4835C17.3139 18.4835 16.5778 19.2353 16.5778 20.1639C16.5778 21.0815 17.3139 21.8333 18.2231 21.8333C19.1216 21.8333 19.8577 21.0815 19.8577 20.1639C19.8577 19.2353 19.1216 18.4835 18.2231 18.4835Z" fill="#0DBA8A" />
+                        </svg>
+                    </a> -->
+                </div>
+            </div>
+            <div class="wrap-content">
+                <?php
+                while ($rows = mysqli_fetch_assoc($res)) :
+                ?>
+                    <a href="martDetail.php?mart=<?= $rows["merchant_id"] ?>">
+                        <div class="mart-list">
+                            <img src="../../uploads/<?= $rows["image"] ?>" alt="" width="100%" height="100%">
+                            <div class="desc">
+                                <h3><?= $rows["name"] ?></h3>
+                                <p><?= $rows["description"] ?></p>
+                            </div>
+                        </div>
+                    </a>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
